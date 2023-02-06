6ca6f72aec5208fd8d3af4de1aa3d7cee6648e08
EC2 Default User <ec2-user@ip-172-31-45-190.ap-northeast-1.compute.internal>
2023-02-06 08:28:11 +0000

    (仮)詳細画面終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,26 @@
 class BooksController < ApplicationController
   def top
   end
-
-  def new
-  end
   
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
   end
 
   def index
+     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
   
-  privete
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get 'top' => 'books#top'
-  post 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+ 
+ resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
